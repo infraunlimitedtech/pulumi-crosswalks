a1c81ce936a799ca16d1c14200e0db468dca0f59
Spigell <spigelly@gmail.com>
2021-12-01 21:36:04 +0300

    :sparkles: Add my github Organization
    GL URL: #8

---
+++ 01-identity/github/organization.go
@@ -0,0 +1,31 @@
+package github
+
+import (
+	"fmt"
+	"github.com/pulumi/pulumi-github/sdk/v4/go/github"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+var users = []map[string]string{
+	{
+		"username": "spigell",
+		"role":     "admin",
+	},
+	{
+		"username": "infraunlimitedBot",
+		"role":     "member",
+	},
+}
+
+func ManageOrganization(ctx *pulumi.Context) error {
+	for _, v := range users {
+		_, err := github.NewMembership(ctx, fmt.Sprintf("orgUser_%s_%s", v["username"], v["role"]), &github.MembershipArgs{
+			Role:     pulumi.String(v["role"]),
+			Username: pulumi.String(v["username"]),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- 01-identity/main.go
+++ 01-identity/main.go
@@ -21,11 +21,23 @@ type CredetialsConfig struct {
 	PublicKey  string
 }
 
+type GithubConfig struct {
+	Managed bool
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		var sshCfg SSHConfig
 		cfg := config.New(ctx, "")
+
+		var sshCfg SSHConfig
 		cfg.RequireObject("ssh", &sshCfg)
+
+		var githubCfg GithubConfig
+		cfg.RequireObject("github", &githubCfg)
+
+		if githubCfg.Managed {
+			_ = github.ManageOrganization(ctx)
+		}
 
 		if sshCfg.ServerAccess.UploadToGithub {
 			decoded, err := base64.StdEncoding.DecodeString(sshCfg.ServerAccess.Credentials.PublicKey)
